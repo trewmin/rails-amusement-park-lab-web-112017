d13e77ff1fb97418afe8ca5002b136254869cd6a
trewmin <trumanlahr@gmail.com>
2017-12-21 15:31:40 -0500

    built user controller and user new view and form partial view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end #class end
